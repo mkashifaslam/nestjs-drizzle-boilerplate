0dc3644d37fea144d99df5e418b7a07da6dc9c03
Muhammad Kashif <mkashif.aslam36@gmail.com>
2024-06-09 09:43:43 +0500

    add get users endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DB_CONNECTION } from './database';
+import { users } from './database/schema';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('DATABASE_CONNECTION')
+    private db: DB_CONNECTION,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getUsers() {
+    return this.db.select().from(users);
   }
 }
